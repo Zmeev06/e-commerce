79c5e40510f670409701aac41602d95a410eec44
Sergey <88145578+Zmeev06@users.noreply.github.com>
2023-09-13 16:54:25 +0300

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+const { configure, presets } = require('eslint-kit')
+
+module.exports = configure({
+  allowDebug: process.env.NODE_ENV !== 'production',
+
+  presets: [
+    presets.imports(),
+    presets.node(),
+    presets.prettier(),
+    presets.typescript(),
+    presets.react(),
+  ],
+
+  extend: {
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto',
+        },
+      ],
+    },
+  },
+})
---
+++ README.md
@@ -0,0 +1,11 @@
+# Template for Good-Goose 🧇
+
+React, Typescript, Eslint-kit, Husky
+
+## Husky setup
+
+    yarn prepare
+
+## VSCode [configuration](https://yarnpkg.com/getting-started/editor-sdks) for yarn 3.x
+
+    yarn dlx @yarnpkg/sdks vscode
